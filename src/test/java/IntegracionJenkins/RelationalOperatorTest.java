1881abf6030ef211d2c7bee66555b315042a34d6
hugo wong <whugo43@hotmail.com>
2020-07-30 22:34:06 -0500

    Update RelationalOperatorTest.java

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -21,12 +21,30 @@ public class RelationalOperatorTest {
 	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertTrue(tester.isLess(2, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+	
+	@Test
+	public void notEqualTest1() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.notEqual(3, 1));
+	}
+	
+	@Test
+	public void notEqualTest2() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.notEqual(2, 2));
+	}
+	
+	@Test
+	public void isGoodHeigth1() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.goodHeight(9,6,13));
 	}
 }
